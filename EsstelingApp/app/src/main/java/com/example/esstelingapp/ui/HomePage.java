c5f7113c26123b5848a9253fb2660d0de5cc4785
Conner Linskens <cmh.linskens@student.avans.nl>
2020-05-26 16:11:54 +0200

    [Addition] Finished the layout of the home page (placeholder text)
    The layout is finished and the progress bars and fact text are editable in the home page. For now these are filled with placeholders.

--- EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/HomePage.java
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/HomePage.java
@@ -4,29 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.esstelingapp.R;
-import com.jackandphantom.blurimage.BlurImage;
 
 public class HomePage extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.home_page, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Finding the background image and making it blurry
-        ImageView backgroundImage = (ImageView) getView().findViewById(R.id.background_image);
-        BlurImage.with(getContext()).load(R.drawable.home_background_image).intensity(20).Async(true).into(backgroundImage);
+        // Finding the random fact text and filling it with a fact
+        TextView randomFactText = (TextView) getView().findViewById(R.id.random_fact_text);
+        // TODO make it actually take a random fact from a file
+        randomFactText.setText("De Essteling geeft niet om klantvriendelijkheid.");
+
+        // Finding the progress bars and setting it a random value
+        ProgressBar storyProgressBar = (ProgressBar) getView().findViewById(R.id.story_progressBar);
+        ProgressBar achievementProgressBar = (ProgressBar) getView().findViewById(R.id.achievement_progressBar);
+        // TODO connect it to the user data and calculate progress
+        storyProgressBar.setProgress(80);
+        achievementProgressBar.setProgress(33);
     }
 }
