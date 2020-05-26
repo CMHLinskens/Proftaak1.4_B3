e7f5146273e51874e0953c5a679db5bcdcacba46
N0cturn4ll <dogukan-akyazi@hotmail.com>
2020-05-26 15:13:16 +0200

    [ADDITION] added activity_settings.xml and made the layout for it. [ADDITION] Added code in the class SettingsPage to make the layout function properly.

--- EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/SettingsPage.java
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/SettingsPage.java
@@ -1,7 +1,58 @@
 package com.example.esstelingapp.ui;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class SettingsPage extends Fragment {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.example.esstelingapp.R;
+
+public class SettingsPage extends AppCompatActivity {
+
+    private RadioGroup group;
+    private RadioButton radioButton;
+    private TextView txtView1;
+    private TextView txtView2;
+    private ToggleButton button;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        this.button = findViewById(R.id.settings_colour_blind_button);
+        this.group = findViewById(R.id.settings_radio_buttons);
+
+    }
+
+    public void onClick(View view) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(button.isChecked()){
+                    Log.d("CLICKABLE BUTTON", "");
+                } else {
+                    Log.d("CLICKABLE BUTTON", "Button has turned off!");
+                }
+            }
+        });
+    }
+
+    public void checkButton(View view) {
+        int radioId = group.getCheckedRadioButtonId();
+
+        radioButton = findViewById(radioId);
+
+        Toast.makeText(this, "Language has changed to: " + radioButton.getText(), Toast.LENGTH_SHORT).show();
+    }
 }
+
