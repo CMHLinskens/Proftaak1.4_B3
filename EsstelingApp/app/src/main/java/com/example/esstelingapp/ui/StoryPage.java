b2c7f088cfb2383de861add61259c76c176f06d2
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2020-05-26 19:58:36 +0200

    [Addition] Added incompleted pop-up functionality

--- EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/StoryPage.java
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/StoryPage.java
@@ -1,7 +1,41 @@
 package com.example.esstelingapp.ui;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class StoryPage extends Fragment {
+import com.example.esstelingapp.R;
 
+public class StoryPage extends Fragment  {
+    private Button button;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.story_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button = (Button) getView().findViewById(R.id.testButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPopup();
+            }
+        });
+    }
+
+    private void openPopup() {
+        StoryUnlockPopup storyUnlockPopup = new StoryUnlockPopup();
+        storyUnlockPopup.show(getFragmentManager(), "Unlock Code");
+    }
 }
