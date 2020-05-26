de3951ccacdcc47bb69ff381ed9dfcf73357dabc
Conner Linskens <cmh.linskens@student.avans.nl>
2020-05-26 11:21:36 +0200

    [Addition] Added the all the UI classes to be filled in

---
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/AchievementPage.java
@@ -0,0 +1,7 @@
+package com.example.esstelingapp.ui;
+
+import androidx.fragment.app.Fragment;
+
+public class AchievementPage extends Fragment {
+
+}
---
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/SettingsPage.java
@@ -0,0 +1,7 @@
+package com.example.esstelingapp.ui;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsPage extends Fragment {
+
+}
