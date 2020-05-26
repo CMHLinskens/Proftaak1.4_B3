9caf937b90639312262c13ff8823f038b1874c0e
Conner Linskens <cmh.linskens@student.avans.nl>
2020-05-26 17:40:04 +0200

    [broken] take it over bart

---
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/StoryUnlockPopup.java
@@ -0,0 +1,24 @@
+package com.example.esstelingapp.ui;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.example.esstelingapp.R;
+
+public class StoryUnlockPopup extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(, null);
+    }
+}
