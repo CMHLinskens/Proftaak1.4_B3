e6dae6f875b99506fa5b73bd8a7196885456d02d
N0cturn4ll <dogukan-akyazi@hotmail.com>
2020-05-27 23:12:15 +0200

    [ADDITION] Made the feature switching themes an actual feature, it now works.

--- EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/SettingsPage.java
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/SettingsPage.java
@@ -1,12 +1,15 @@
 package com.example.esstelingapp.ui;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -20,6 +23,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.example.esstelingapp.R;
 
 public class SettingsPage extends AppCompatActivity {
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_COLOUR_BLIND_THEME = "colour_blind_theme";
+
 
     private ConstraintLayout layout;
     private RadioGroup group;
@@ -27,39 +33,56 @@ public class SettingsPage extends AppCompatActivity {
     private TextView txtView1;
     private TextView txtView2;
     private ToggleButton button;
+    private boolean colourBlindTheme;
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean useColourBlindTheme = preferences.getBoolean(PREF_COLOUR_BLIND_THEME, false);
+        if (useColourBlindTheme) {
+            setTheme(R.style.ColourBlindTheme);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        this.layout = findViewById(R.id.settings_layout);
         this.button = findViewById(R.id.settings_colour_blind_button);
+        this.layout = findViewById(R.id.settings_layout);
         this.txtView1 = findViewById(R.id.settings_colour_blind);
         this.txtView2 = findViewById(R.id.settings_language);
         this.group = findViewById(R.id.settings_radio_buttons);
-    }
 
-    public void onClick(View view) {
-        int radioId = group.getCheckedRadioButtonId();
+        this.button.setChecked(useColourBlindTheme);
 
-        radioButton = findViewById(radioId);
-
-        view.setOnClickListener(new View.OnClickListener() {
+        this.button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                if(button.isChecked()){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
                     Log.d("CLICKABLE BUTTON", "Button has turned on!");
+                    toggleColourBlindMode(true);
 
-//                    getTheme().applyStyle(R.style.ColourBlindTheme, true);
-                    getApplication().setTheme(R.style.ColourBlindTheme);
                 } else {
                     Log.d("CLICKABLE BUTTON", "Button has turned off!");
+                    toggleColourBlindMode(false);
                 }
             }
         });
     }
+
+    private void toggleColourBlindMode(boolean colourBlindTheme) {
+        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putBoolean(PREF_COLOUR_BLIND_THEME, colourBlindTheme);
+        editor.apply();
+
+        finish();
+        Intent intent = getIntent();
+        startActivity(intent);
+    }
+
+    public void onClick(View view) {
+
+    }
+
 
     public void checkButton(View view) {
         int radioId = group.getCheckedRadioButtonId();
