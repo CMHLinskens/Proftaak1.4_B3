23bbd83c0ddc077df042f502372501e654ec5c2d
Conner Linskens <cmh.linskens@student.avans.nl>
2020-05-28 11:00:58 +0200

    [Addition] Created JSonReader class

---
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/json/JSonReader.java
@@ -0,0 +1,18 @@
+package com.example.esstelingapp.json;
+
+import android.util.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JSonReader {
+    public static void readAllJsonFiles(){
+        readQuizFile();
+    }
+
+    private static void readQuizFile(){
+        String jsonString = 
+    }
+
+
+}
