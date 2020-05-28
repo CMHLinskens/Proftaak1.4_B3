eb304181029b5ec2429908980288ef1e4c085092
N0cturn4ll <dogukan-akyazi@hotmail.com>
2020-05-28 10:58:14 +0200

    [ADDITION] Tried to convert SettingsPage to a fragment.

--- EsstelingApp/app/src/main/java/com/example/esstelingapp/MainActivity.java
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.esstelingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+import com.example.esstelingapp.ui.SettingsPage;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String PREFS_NAME = "prefs";
+    private static final String PREF_COLOUR_BLIND_THEME = "colour_blind_theme";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean useColourBlindTheme = preferences.getBoolean(PREF_COLOUR_BLIND_THEME, false);
+        if (useColourBlindTheme) {
+            setTheme(R.style.ColourBlindTheme);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
--- EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/SettingsPage.java
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/ui/SettingsPage.java
@@ -8,7 +8,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.JsonReader;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -16,45 +18,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
 
 import com.example.esstelingapp.R;
 
-public class SettingsPage extends AppCompatActivity {
+import static android.content.Context.MODE_PRIVATE;
+
+public class SettingsPage extends Fragment {
     private static final String PREFS_NAME = "prefs";
     private static final String PREF_COLOUR_BLIND_THEME = "colour_blind_theme";
 
-
-    private ConstraintLayout layout;
-    private RadioGroup group;
-    private RadioButton radioButton;
-    private TextView txtView1;
-    private TextView txtView2;
-    private ToggleButton button;
-    private boolean colourBlindTheme;
+    private ToggleButton toggleButton;
 
 
+    @Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        boolean useColourBlindTheme = preferences.getBoolean(PREF_COLOUR_BLIND_THEME, false);
-        if (useColourBlindTheme) {
-            setTheme(R.style.ColourBlindTheme);
-        }
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
-        this.button = findViewById(R.id.settings_colour_blind_button);
-        this.layout = findViewById(R.id.settings_layout);
-        this.txtView1 = findViewById(R.id.settings_colour_blind);
-        this.txtView2 = findViewById(R.id.settings_language);
-        this.group = findViewById(R.id.settings_radio_buttons);
-
-        this.button.setChecked(useColourBlindTheme);
-
-        this.button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        this.toggleButton = getActivity().findViewById(R.id.settings_colour_blind_button);
+        this.toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
@@ -67,30 +52,18 @@ public class SettingsPage extends AppCompatActivity {
                 }
             }
         });
+        return inflater.inflate(R.layout.activity_settings, container, false);
     }
 
     private void toggleColourBlindMode(boolean colourBlindTheme) {
-        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        SharedPreferences.Editor editor = getActivity().getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
         editor.putBoolean(PREF_COLOUR_BLIND_THEME, colourBlindTheme);
         editor.apply();
 
-        finish();
-        Intent intent = getIntent();
+        getActivity().finish();
+        Intent intent = getActivity().getIntent();
+        intent.putExtra("colourBlind_theme", colourBlindTheme);
         startActivity(intent);
     }
-
-    public void onClick(View view) {
-
-    }
-
-
-    public void checkButton(View view) {
-        int radioId = group.getCheckedRadioButtonId();
-
-        radioButton = findViewById(radioId);
-
-        Toast.makeText(this, "Language has changed to: " + radioButton.getText(), Toast.LENGTH_SHORT).show();
-    }
-
 }
 
