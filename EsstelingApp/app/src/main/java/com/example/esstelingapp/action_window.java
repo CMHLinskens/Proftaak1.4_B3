c58a670289e7c6bb7e18f9689a941a7d56a3c68b
Erwin <Erwinvanhunnik@gmail.com>
2020-05-29 13:56:45 +0200

    [Addition] Added actionwindow

---
+++ EsstelingApp/app/src/main/java/com/example/esstelingapp/action_window.java
@@ -0,0 +1,15 @@
+package com.example.esstelingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class action_window extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_window);
+    }
+
+}
