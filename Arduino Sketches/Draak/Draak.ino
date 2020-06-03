5494fb0ad3eff991b21975ababd7a1851d4a473d
Conner Linskens <cmh.linskens@student.avans.nl>
2020-06-03 14:07:35 +0200

    [Addition] LED actuator now works with PWM. Still needs calibration tho

--- Arduino Sketches/Draak/Draak.ino
+++ Arduino Sketches/Draak/Draak.ino
@@ -1,24 +1,41 @@
 #define PIN_TRIGGER 27
 #define PIN_ECHO 26
+#define PIN_LED 32
 
-float detectionRange = 50.0f;
+
+// Ultrasone variables
+const static float detectionRange = 50.0f;
 float distance = 0.0f;
 bool isDetecting = false;
+
+
+// PWM variables
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
+const int signalAmplifier = 1500;
 
 void setup(){
   Serial.begin(9600);
   pinMode(PIN_TRIGGER, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
+
+  // Do the setup for the PWM led
+  ledcSetup(ledChannel, freq, resolution);
+
+  // Attach the led pin
+  ledcAttachPin(PIN_LED, ledChannel);
 }
 
 void loop() {
   startUltrasone();
   distance = getDetectedDistance();
   isDetecting = checkIfInRange(distance);
-  
-  Serial.print(isDetecting);
-  Serial.print('\t');
-  Serial.println(distance);
+
+  // if the ultrasone is detecting then calculate the PWM value
+  int ledStrength = isDetecting? constrain(((1/distance)*signalAmplifier), 0, 255) : 0;
+  Serial.println(ledStrength);
+  ledcWrite(ledChannel, ledStrength);
   
   delay(100);
 }
